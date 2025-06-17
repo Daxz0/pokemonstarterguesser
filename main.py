b1e040de66bbd9e3a87d2543eb6dd8fab45facc5
Tfu1234 <ty.fu@outlook.com>
2025-06-17 00:14:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+#libraries
 import pandas as pd
 import random
 import time
+from collections import Counter
 
-#Start
-print("HELLO AND WELCOME TO THE POKEMON STARTER, GUESSING GAME!")
-print("THE CATCH IS THAT YOU, WON'T BE THE ONE GUESSING")
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
-time.sleep(1)
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import model_selection
+from sklearn.metrics import accuracy_score
+
+import tensorflow.keras as keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, InputLayer
+from keras.layers import Activation, MaxPooling2D, Dropout, Flatten, Reshape
+from keras.utils import to_categorical
+
+
+cnn = CNNClassifier(num_epochs, layers, dropout)
