94c45dfaacccb026944ceb85b74113e8a31832c3
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-06-16 23:06:09 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,2 +1,5 @@
-INPUT_PATH = "data"
-OUTPUT_PATH = "converted_data"
+class Constants:
+    def __init__(self):
+        self.MAX_NEAREST_NEIGHBORS = 21
+        self.TEST_SPLIT = 0.2
+        self.RANDOM_STATE = 1
