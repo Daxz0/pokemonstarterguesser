f1aa79f0fbb445bef45f5230b0d11d9cee018569
Daxz <59543649+Daxz0@users.noreply.github.com>
2025-06-16 21:22:46 -0700

    + PIL for resolution lowering

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import random
 import time
 from collections import Counter
+from PIL import Image
 
 import numpy as np
 import pandas as pd
@@ -21,4 +22,20 @@ from keras.layers import Activation, MaxPooling2D, Dropout, Flatten, Reshape
 from keras.utils import to_categorical
 
 
-cnn = CNNClassifier(num_epochs, layers, dropout)
+# cnn = CNNClassifier(num_epochs, layers, dropout)
+
+print("test")
+
+
+# def load_data():
+#   !wget -q --show-progress -O cifar_data https://storage.googleapis.com/inspirit-ai-data-bucket-1/Data/AI%20Scholars/Sessions%201%20-%205/Session%204%20_%205%20-%20Neural%20Networks%20_%20CNN/dogs_v_roads
+
+#   import pickle
+#   data_dict = pickle.load(open( "cifar_data", "rb" ));
+
+#   data   = data_dict['data']
+#   labels = data_dict['labels']
+
+#   return data, labels
+
+# data, labels = load_data()
